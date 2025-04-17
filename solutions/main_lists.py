ccaa04e8ad3df71779098ba86df04c0bfaf4ebe2
Hari-42 <harisaranmohanathas@hotmail.com>
2025-04-17 23:58:25 +0200

    added second exercise solution

--- solutions/main_lists.py
+++ solutions/main_lists.py
@@ -5,3 +5,13 @@ def simplelist():
     animals.append('Zebra')
     animals.append('Leopard')
     return animals
+
+def givecarbrandnames():
+    carbrands = []
+    i = 0
+    while i < 5:
+        brand = input("carbrand > ")
+        carbrands.append(brand)
+        i += 1
+    index = int(input("Name a number from 1-5 > "))
+    print(carbrands[index - 1])
