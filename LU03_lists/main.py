5d2b20346df231c632dab8da0f219c54e0b48db9
Hari-42 <harisaranmohanathas@hotmail.com>
2025-04-17 23:57:52 +0200

    added second exercise

--- LU03_lists/main.py
+++ LU03_lists/main.py
@@ -6,3 +6,11 @@ def simplelist():
     # TODO: Add an another animal called Leopard
     # TODO: Return the list
     pass
+
+
+def givecarbrandnames():
+    # TODO: Create an empty list called carbrands
+    # TODO: Allow the user, to only give 5 carbrands
+    # TODO: let the user choose from 1 to 5
+    # Todo: print the preferred indexed carbrand
+    pass
--- LU03_lists/main_test.py
+++ LU03_lists/main_test.py
@@ -1,5 +1,15 @@
-from LU03_lists.main import simplelist
+from LU03_lists.main import simplelist, givecarbrandnames
 
 def test_simplelist():
     result = simplelist()
     assert result == list(('Tiger', 'Lion', 'Zebra', 'Leopard'))
+
+
+def test_givecarbrandnames(monkeypatch, capsys):
+    inputs = iter(['BMW', 'Toyota', 'Nissan', 'Audi', 'Porsche', '3'])  # '3' as string
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+    givecarbrandnames()
+    captured = capsys.readouterr()
+
+    assert "Nissan" in captured.out
