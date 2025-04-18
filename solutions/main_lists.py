c8c79a789a0d901bb154560a9f1c8e1fa1cc4710
Hari-42 <harisaranmohanathas@hotmail.com>
2025-04-18 12:53:16 +0200

    Learning unit 3 soultion added

--- solutions/main_lists.py
+++ solutions/main_lists.py
@@ -15,3 +15,37 @@ def givecarbrandnames():
         i += 1
     index = int(input("Name a number from 1-5 > "))
     print(carbrands[index - 1])
+
+def add_countries(countries):
+    countries.append('Portugal')
+    countries.insert(3, 'Denmark')
+    print('add_countries:\n', countries)
+
+
+def remove_countries(countries):
+    countries.pop()
+    countries.pop(1)
+    countries.remove('Italy')
+    print('remove_countries:\n', countries)
+
+
+def find_countries(countries):
+    print('find_country:')
+    print(countries[5])
+    print(countries.index('Germany'))
+
+
+def loop_countries(countries):
+    print('loop_countries:')
+    number = 1
+    for country in countries:
+        print(f'Nr. {number}: {country}')
+        number += 1
+
+
+def sort_countries(countries):
+    print('sort_countries:')
+    number = len(countries)
+    for country in sorted(countries, reverse =True):
+        print(f'Nr. {number}: {country}')
+        number -= 1
