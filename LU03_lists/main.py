2942cce62771b1f3812bd6243888f5fcbcef0365
Hari-42 <harisaranmohanathas@hotmail.com>
2025-04-18 12:51:07 +0200

    Learning unit 3 third task and test

--- LU03_lists/main.py
+++ LU03_lists/main.py
@@ -14,3 +14,45 @@ def givecarbrandnames():
     # TODO: let the user choose from 1 to 5
     # Todo: print the preferred indexed carbrand
     pass
+
+
+def countrieslist():
+    countries = ['Germany', 'France', 'Italy', 'Austria', 'Spain', 'Netherlands', 'Belgium', 'Ukraine']
+    add_countries(countries)
+
+    countries = ['Germany', 'France', 'Italy', 'Austria', 'Spain', 'Netherlands', 'Belgium', 'Ukraine']
+    remove_countries(countries)
+
+    countries = ['Germany', 'France', 'Italy', 'Austria', 'Spain', 'Netherlands', 'Belgium', 'Ukraine']
+    find_countries(countries)
+    loop_countries(countries)
+    sort_countries(countries)
+
+def add_countries(countries):
+    # TODO: add Portugal at the end of the list
+    # TODO: add Denmark at Index 3
+    print('add_countries:\n', countries)
+
+
+def remove_countries(countries):
+    # TODO: remove the last country from the list
+    # TODO: remove the country at Index 1
+    # TODO: remove Italy from the list
+    print('remove_countries:\n', countries)
+
+
+def find_countries(countries):
+    print('find_country:')
+    # TODO: print the country at Index 5
+    # TODO: print the position of Germany
+    pass
+
+
+def loop_countries(countries):
+    print('loop_countries:')
+    # TODO: print all countries in the list. output must be 'Nr. x: countryname', for example 'Nr. 1: Germany'
+
+
+def sort_countries(countries):
+    print('sort_countries:')
+    # TODO: print all countries ordered by Name (descending). output must be 'Nr. x: countryname', for example 'Nr. 8: Ukraine'
--- LU03_lists/main_test.py
+++ LU03_lists/main_test.py
@@ -1,4 +1,4 @@
-from LU03_lists.main import simplelist, givecarbrandnames
+from LU03_lists.main import simplelist, givecarbrandnames, add_countries, remove_countries, find_countries, loop_countries, sort_countries
 
 def test_simplelist():
     result = simplelist()
@@ -13,3 +13,59 @@ def test_givecarbrandnames(monkeypatch, capsys):
     captured = capsys.readouterr()
 
     assert "Nissan" in captured.out
+
+
+def test_add(monkeypatch, capsys):
+    countries = ['Germany', 'France', 'Italy', 'Austria', 'Spain', 'Netherlands', 'Belgium', 'Ukraine']
+    add_countries(countries)
+    captured = capsys.readouterr()
+    assert captured.out == "add_countries:\n ['Germany', 'France', 'Italy', 'Denmark', 'Austria', 'Spain', 'Netherlands', 'Belgium', 'Ukraine', 'Portugal']\n"
+
+
+def test_remove(monkeypatch, capsys):
+    countries = ['Germany', 'France', 'Italy', 'Austria', 'Spain', 'Netherlands', 'Belgium', 'Ukraine']
+    remove_countries(countries)
+    captured = capsys.readouterr()
+    assert captured.out == "remove_countries:\n ['Germany', 'Austria', 'Spain', 'Netherlands', 'Belgium']\n"
+
+
+
+def test_find(monkeypatch, capsys):
+    countries = ['Germany', 'France', 'Italy', 'Austria', 'Spain', 'Netherlands', 'Belgium', 'Ukraine']
+    find_countries(countries)
+    captured = capsys.readouterr()
+    assert captured.out == "find_country:\nNetherlands\n0\n"
+
+
+def test_loop(monkeypatch, capsys):
+    countries = ['Germany', 'France', 'Italy', 'Austria', 'Spain', 'Netherlands', 'Belgium', 'Ukraine']
+    loop_countries(countries)
+    captured = capsys.readouterr()
+    assert captured.out == (
+        "loop_countries:\n"
+        "Nr. 1: Germany\n"
+        "Nr. 2: France\n"
+        "Nr. 3: Italy\n"
+        "Nr. 4: Austria\n"
+        "Nr. 5: Spain\n"
+        "Nr. 6: Netherlands\n"
+        "Nr. 7: Belgium\n"
+        "Nr. 8: Ukraine\n"
+    )
+
+
+def test_sort(monkeypatch, capsys):
+    countries = ['Germany', 'France', 'Italy', 'Austria', 'Spain', 'Netherlands', 'Belgium', 'Ukraine']
+    sort_countries(countries)
+    captured = capsys.readouterr()
+    assert captured.out == (
+        "sort_countries:\n"
+        "Nr. 8: Ukraine\n"
+        "Nr. 7: Spain\n"
+        "Nr. 6: Netherlands\n"
+        "Nr. 5: Italy\n"
+        "Nr. 4: Germany\n"
+        "Nr. 3: France\n"
+        "Nr. 2: Belgium\n"
+        "Nr. 1: Austria\n"
+    )
